9825fa975b4e3fb6d2c191e310d933cf1b3b123f
tfdx29 <eyeofsouran69@gmail.com>
2022-04-06 21:33:49 +0545

    Initial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1992a571cbe9d0963ce5140aec8c369b'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'aeac80c4-050f-4be8-a2ce-44fb2e77ccf2'
+};
